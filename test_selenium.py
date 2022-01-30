1fb3916f07232a76c0cce20c25a28c6e56aebda4
super-pylori <super.pylori6@gmail.com>
2022-01-30 23:15:08 +0900

    i

--- test_selenium.py
+++ test_selenium.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
 
 op = Options()
 # ヘッドレスモード（Linux上で動かすとき必ずこのモードにしておく）
@@ -23,6 +27,37 @@ op.add_experimental_option('excludeSwitches', ['enable-logging'])
 op.add_experimental_option('useAutomationExtension', False)
 # op.add_experimental_option("detach", True) # ブラウザ出るモードの時処理終了後もブラウザを閉じない
 op.page_load_strategy = 'eager'
-driver = webdriver.Chrome(ChromeDriverManager().install(),options=op)
-driver.get('https://example.com')
-print(driver.current_url)
+# driver = webdriver.Chrome(ChromeDriverManager().install(),options=op)
+# driver.get('https://example.com')
+# print(driver.current_url)
+
+# （A表）先月から過去1年間の当せん番号　のリンクを格納した配列を返す
+def get_tableA_links():
+    for i in range(0, 3):
+        try:
+            driver = webdriver.Chrome(ChromeDriverManager().install(),options=op)
+        except Exception as e:
+            print(e)
+            time.sleep(5)
+        else:
+            break
+
+    url = 'https://www.mizuhobank.co.jp/retail/takarakuji/check/numbers/backnumber/index.html'
+    driver.get(url)
+
+    try:
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "ナンバーズ3")))
+    except Exception as e:
+        time.sleep(10)
+
+    lists = []
+    links = driver.find_elements_by_partial_link_text('ナンバーズ3')
+    for link in links:
+        lists.append(link.get_attribute('href'))
+        if len(lists) == 12:
+            break 
+    return lists
+
+
+tableA_links = get_tableA_links()
+print(tableA_links)
