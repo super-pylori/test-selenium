da64f47caeb213113552703923de2ee6921a0356
super-pylori <super.pylori6@gmail.com>
2022-01-30 23:32:36 +0900

    j

--- test_selenium.py
+++ test_selenium.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
 
 import time
 
@@ -27,9 +28,6 @@ op.add_experimental_option('excludeSwitches', ['enable-logging'])
 op.add_experimental_option('useAutomationExtension', False)
 # op.add_experimental_option("detach", True) # ブラウザ出るモードの時処理終了後もブラウザを閉じない
 op.page_load_strategy = 'eager'
-# driver = webdriver.Chrome(ChromeDriverManager().install(),options=op)
-# driver.get('https://example.com')
-# print(driver.current_url)
 
 # （A表）先月から過去1年間の当せん番号　のリンクを格納した配列を返す
 def get_tableA_links():
@@ -58,6 +56,128 @@ def get_tableA_links():
             break 
     return lists
 
+# A表内リンクから回別、抽選日、抽選数字をDataFrameに変換
+def get_data_from_tableA(tableA_links):
+    for i in range(0, 3):
+        try:
+            driver = webdriver.Chrome(ChromeDriverManager().install(),options=op)
+        except Exception as e:
+            print(e)
+            time.sleep(5)
+        else:
+            break
+
+    backnumbers = []
+    for link in tableA_links:
+        url = link
+        print(url)
+
+        driver.get(url)
+
+        try:
+            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[text()='ストレート']")))
+        except Exception as e:
+            time.sleep(10)        
+        
+        no = driver.find_elements_by_class_name('bgf7f7f7')
+        date = driver.find_elements_by_class_name('js-lottery-date-pc')
+        number = driver.find_elements_by_class_name('js-lottery-number-pc')
+
+        for i in range(0,len(no)):
+            backnumber = {}
+            backnumber['回別'] = no[i].text
+            backnumber['抽せん日'] = date[i].text
+            backnumber['ナンバーズ3抽せん数字'] = number[i].text
+            backnumbers.append(backnumber)
+
+    return backnumbers
+
+# （B表）A表以前の当せん番号　のリンクを格納した配列を返す
+def get_tableB_links():
+    for i in range(0, 3):
+        try:
+            driver = webdriver.Chrome(ChromeDriverManager().install(),options=op)
+        except Exception as e:
+            print(e)
+            time.sleep(5)
+        else:
+            break
+
+    url = 'https://www.mizuhobank.co.jp/retail/takarakuji/check/numbers/backnumber/index.html' 
+    driver.get(url)
+
+    try:
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "第1回〜第20回")))
+    except Exception as e:
+        time.sleep(10)
+
+    lists = []
+    links = driver.find_elements_by_xpath("//*[@class='js-backnumber-temp-b']/td/a")
+    for link in links:
+        if link.get_attribute('href') == "":
+            continue
+        lists.append(link.get_attribute('href'))
+    
+    return lists
+
+# B表内リンクから回別、抽選日、抽選数字をDataFrameに変換
+def get_data_from_tableB(tableB_links):
+    for i in range(0, 3):
+        try:
+            driver = webdriver.Chrome(ChromeDriverManager().install(),options=op)
+        except Exception as e:
+            print(e)
+            time.sleep(5)
+        else:
+            break
+
+    backnumbers = []
+    for link in tableB_links:
+        url = link
+        print(url)
+
+        driver.get(url)
+
+        try:
+            WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='mainCol']/article/div[1]/div/div[1]/table/tbody/tr[20]/th")))
+        except Exception as e:
+            time.sleep(10)
+
+        # html = driver.page_source
+        # with open('hoge.html', 'w', encoding='utf-8') as f:
+        #     f.write(html)
+
+        no = driver.find_elements_by_xpath("//table[@class='typeTK']/tbody/tr/th[@class='bgf7f7f7']")
+        date = driver.find_elements_by_xpath("//table[@class='typeTK']/tbody/tr/th[@class='bgf7f7f7']/following-sibling::td[1]")
+        number = driver.find_elements_by_xpath("//table[@class='typeTK']/tbody/tr/th[@class='bgf7f7f7']/following-sibling::td[2]")
+
+        for i in range(0,len(no)):
+            backnumber = {}
+            backnumber['回別'] = no[i].text
+            backnumber['抽せん日'] = date[i].text
+            backnumber['ナンバーズ3抽せん数字'] = number[i].text
+            backnumbers.append(backnumber)
+
+    return backnumbers[::-1]
 
 tableA_links = get_tableA_links()
-print(tableA_links)
+backnumbersA = get_data_from_tableA(tableA_links)
+backnumbersA_df = pd.DataFrame(backnumbersA)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(backnumbersA_df)
+
+exit()
+
+tableB_links = get_tableB_links()
+backnumbersB = get_data_from_tableB(tableB_links)
+backnumbersB_df = pd.DataFrame(backnumbersB)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(backnumbersB_df)
+
+df = pd.DataFrame()
+df = pd.concat([backnumbersA_df,backnumbersB_df], ignore_index=True)
+
+# 空白行の削除
+df = df.dropna(how='all')
+
+df.to_csv('numbers3.csv')
